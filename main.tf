7fae861f1ec28bb02c04c1a1257d889e371cd078
AngelineTung <angelinetung@hotmail.com>
2025-11-05 21:01:04 +0800

    Add code

---
+++ main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
+  description = "AWS region"
+}
+
+# Package the lambda directory into a zip
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda"
+  output_path = "${path.module}/lambda.zip"
+}
+
+# Execution role for Lambda
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name               = "hello-world-lambda-exec"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+# Basic logging permissions
+resource "aws_iam_role_policy_attachment" "basic_exec" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "hello" {
+  function_name = "hello-world"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "handler.handler"
+  runtime       = "python3.12"
+  filename      = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  timeout       = 5
+}
---
+++ variables.tf
@@ -0,0 +1,1 @@
+output "lambda_name" { value = aws_lambda_function.hello.function_name }
